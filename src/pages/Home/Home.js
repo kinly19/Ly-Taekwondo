d5d48fdfcceca1e512da31b8dfefbb6ad254d719
unknown <kinchoily@hotmail.com>
2020-07-28 22:07:25 +0100

    Home page layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,9 +3,49 @@ import './Home.scss';
 
 const Home = () => {
     return (
-        <div className='Home'>
-            <h1>Welcome to Home page</h1>
+
+        //container to hold main layout css grid
+        <div className="layout__container">
+
+            <div className="header">
+                <h1 className="header__title">Ly Taekwondo</h1>
+                <h3 className="header__subtitle">Timeless Life Skills</h3>
+                <button className="btn1">join</button>
+            </div>
+
+            <div className="section_welcome">
+                <h1>Section 2</h1>
+            </div>
+
+            <div className="section_workshop">
+                <h1>Section 3</h1>
+            </div>
+
+            <div className="section_testimony">
+                <h1>Section 4</h1>
+            </div>
+
+
+            {/* //header for the top 
+
+            //main 
+            
+                
+                //section (welcome page)
+
+                //section (trainning programms)
+
+                //section (Testimonies)
+
+                //section (Gallery)
+
+            //main close
+
+            //container close */}
+
         </div>
+
+
 
     )
 }
