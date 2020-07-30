45ad9ccd6aba3a91ddda7814886ebdcf6259012f
unknown <kinchoily@hotmail.com>
2020-07-30 22:38:34 +0100

    Home markup update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.scss';
+import img2 from '../../assets/img/img2.jpg';
 
 const Home = () => {
     return (
@@ -14,7 +15,33 @@ const Home = () => {
             </div>
 
             <div className="section_welcome">
-                <h1>Section 2</h1>
+
+                <div className="section_welcome__clip">
+                    <img className="section_welcome__img" src={img2} alt="img2"></img>
+                </div>
+
+                <div className="section_welcome__right">
+                    <h1 className="section_welcome__header">Ly Taekwondo</h1>
+                    <p className="section_welcome__paragraph section_welcome__paragraph--1">
+                        360 Kick provides Taekwondo and mindfulness classes,
+                        that helps busy professionals and business owners to
+                        release stress, and develop both physical and mental resilience,
+                        so that they can be more focused and efficient at work, also gives
+                        the mental toughness and agility to deal with day to day challenges
+                        with ease and power
+                    </p>
+
+                    <p className="section_welcome__paragraph section_welcome__paragraph--2">
+                        Blending martial arts and mindfulness helps
+                        individuals deepen the connections with their body
+                        and mind, so that cultivate a healthier relationship
+                        with themselves which plays an important part of ones
+                        physical, mental and financial well-being.
+                    </p>
+
+
+                </div>
+
             </div>
 
             <div className="section_workshop">
@@ -25,27 +52,7 @@ const Home = () => {
                 <h1>Section 4</h1>
             </div>
 
-
-            {/* //header for the top 
-
-            //main 
-            
-                
-                //section (welcome page)
-
-                //section (trainning programms)
-
-                //section (Testimonies)
-
-                //section (Gallery)
-
-            //main close
-
-            //container close */}
-
         </div>
-
-
 
     )
 }
