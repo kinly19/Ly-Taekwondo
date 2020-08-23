738b8185fe2f19de78c094a62ba34f145018f234
unknown <kinchoily@hotmail.com>
2020-08-23 22:14:50 +0100

    slider component update

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import img2 from "../../assets/img/img2.jpg";
 import img3 from "../../assets/img/img3.jpg";
+import sprite from "../../assets/icons/sprite.svg";
 import "./Slider.scss";
 import ImgComp from '../../components/ImgComp'
 
@@ -9,14 +10,28 @@ function Slider() {
     //create an array for componet to show inside the slider
     let sliderArr = [
         <ImgComp
-            src={img3}
-            imgClass=""
-            heading="Ly Taekwondo"
-            subHeading="TimeLess Life Skills"
-        />,
-        <ImgComp className="slider__img" src={img2} />
 
-    ] //numbers for now
+            className="slider__img slider__img--1"
+            headerClass="landing__title"
+            subheaderClass="landing__subtitle"
+            BackgroundImg={img3}
+            header="Ly Taekwondo"
+            subHeader="TimeLess Life Skills"
+
+
+        />,
+
+        <ImgComp
+
+            className="slider__img slider__img--2"
+            headerClass="landing__title"
+            subheaderClass="landing__subtitle"
+            BackgroundImg={img2}
+            header="What is Takwondo"
+            subHeader="TimeLess Life Skills"
+        />
+
+    ]
     const [x, setX] = useState(0) //set x to usestate of 0 and to have setX to set x
 
     //function for left and right buttons
@@ -33,25 +48,33 @@ function Slider() {
     }
 
 
+
     return (
         <div className="slider-container">
-
-
             {
                 sliderArr.map((item, index) => {
                     return (
-                        <div key={index} className="slider" style={{ transform: `translateX(${x}%)` }}>
+                        <div key={index} className="slider" style={{ transform: `translateX(${x}%)` }}> {/**slider block */}
                             {item}
+
                         </div>
                     )
                 })
-
             }
 
             {/*Adding two buttons to navigate*/}
-            <button id="goLeft" onClick={goLeft}>left</button>
-            <button id="goRight" onClick={goRight}>right</button>
-        </div>
+
+            <button className="slider-button slider-button__goLeft" onClick={goLeft}>
+                <svg className="slider-button slider-button__icon">
+                    <use xlinkHref={sprite + "#icon-chevron-thin-left"}></use>
+                </svg>
+            </button>
+            <button className="slider-button slider-button__goRight" onClick={goRight}>
+                <svg className="slider-button slider-button__icon">
+                    <use xlinkHref={sprite + "#icon-chevron-thin-right"}></use>
+                </svg>
+            </button>
+        </div >
     )
 
 }
