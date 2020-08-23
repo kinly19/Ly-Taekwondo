2a8a28696d4f01d71f77ccb55a3e7b06aaa28638
unknown <kinchoily@hotmail.com>
2020-08-23 22:23:26 +0100

    card component dynamic className changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Card.scss';
 
+
+
 const Card = (props) => {
+
     return (
-        <div className="card">
-            <img src={props.image} alt="" />
+        <div className={props.className}>
+            <img className={props.imgClass} src={props.image} alt="" />
             <h1 className="card__heading">{props.title}</h1>
             <p className="card__text"> {props.details}</p>
-            <button className="card__button">{props.buttonTitle}</button>
+            <button className="card__button btn btn--main">{props.buttonTitle}</button>
         </div>
 
     )
