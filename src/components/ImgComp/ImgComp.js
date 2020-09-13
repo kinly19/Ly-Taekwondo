c98770f0f3e65661c332a75d6dcea9b78f6fef9a
unknown <kinchoily@hotmail.com>
2020-09-13 22:04:57 +0100

    minor changes, Added button inside component

--- src/components/ImgComp/ImgComp.js
+++ src/components/ImgComp/ImgComp.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import Button from '../Button';
 import './ImgComp.scss'
 
-function ImgComp({ BackgroundImg, headerClass, subheaderClass, className, header, subHeader }) {
-
-    // let imgStyles = {
-    //     width: 100 + "vw",
-    //     height: 100 + "vh",
-    //     objectFit: "cover",
-    // }
+function ImgComp({ BackgroundImg, className, headerClass, subheaderClass, header, subHeader, activeStyle, buttonClass, buttonTitle, buttonLink }) {
 
     let background = {
         backgroundImage: `linear-gradient(to right bottom, #cc00185e, #c208024d),url(${BackgroundImg})`
     }
 
+    //not needed
+    // let activeClass = {
+    //     display: "block"
+    // }
+
+
     return (
         <div className={className} style={background} > {/**slide*/}
-            {/* <img className={imgClass} src={src} alt="slide-img" style={imgStyles}></img> */}
             <h1 className={headerClass}>{header}</h1>
             <h3 className={subheaderClass}>{subHeader}</h3>
+
+            <div className={activeStyle}> {/**not needed? */}
+                <Button
+                    className={buttonClass}
+                    title={buttonTitle}
+                    link={buttonLink}
+                />
+
+            </div>
+
         </div >
 
     )
