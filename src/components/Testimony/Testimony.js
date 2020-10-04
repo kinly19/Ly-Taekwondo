4a9ce98b812241549950bc86dfff4b8fe126a1c5
unknown <kinchoily@hotmail.com>
2020-10-04 22:43:27 +0100

    Testimony update to span style on active state

--- src/components/Testimony/Testimony.js
+++ src/components/Testimony/Testimony.js
@@ -1,40 +1,46 @@
+
 import React, { useState } from 'react';
 import './Testimony.scss';
+
 
 const Testimony = (props) => {
 
 
     const testimonials = [ //state which will hold our data
         {
-            client: "Ahren",
+            client: "Ahren - Participant of Ly Taekwondo Workshop",
             quote: "After the session I felt revived, it had been a long time since i had a proper work out in class and this worked out both body and mind!"
         },
 
         {
-            client: "Craig",
+            client: "Craig - Participant of Ly Taekwondo Workshop",
             quote: "Friendly, enthusiastic and knowledgeable and manged to keep the experience light hearted and informative. I look forward to the next session."
         },
 
         {
-            client: "Avril",
+            client: "Avril - Participant of Ly Taekwondo Workshop",
             quote: "Feeling re-energised and calm after a long week. It was a great introduction to Taekwondo and some self-defence techniques. Very friendly and helpful, on hand to lead you through the process and aid you in grasping the techniques. Overall I felt I learnt a lot, whilst having fun, getting in a cardio workout and meeting new people. I would definitely recommend it to others!"
         }
     ]
 
-    const [current, setCurrent] = useState(testimonials[0]); //telling react what part of the state(data from the state) we want to use
+    const [current, setCurrent] = useState(testimonials[2]); //telling react what part of the state(data from the state) we want to use
 
-    const [active, setActive] = useState(0); //helps us keep track of span color
-
-    const handleSetClick = (event) => {
-
-
-        setCurrent(testimonials[event.target.getAttribute("data-quote")]) //setCurrent too
-        setActive(event.target.getAttribute("data-quote"))
+    const [active, setActive] = useState(2); //helps us keep track of span color
 
 
 
 
 
+    const handleSetClick = (index) => {
+
+        setCurrent(testimonials[index]) //setCurrent too
+        setActive(index)
+
+    }
+
+
+    const background = {
+        backgroundImage: `url(${props.backgroundImg})`
     }
 
 
@@ -43,27 +49,32 @@ const Testimony = (props) => {
     return (
 
         <div className="testimony">
+            <div className="testimony__img" style={background}></div>
 
-            <h1 className="testimony__title">{current.quote}</h1>
-            <h3 className="testimony__subtitle">{current.client}</h3>
+            <div className="testimony__content">
 
-            <div className="testimony__active">
+                <div className="testimony__content-container">
+                    <h1 className="testimony__header">What Others Had To Say</h1>
+                    <p className="testimony__text">{current.quote}</p> {/* this comes from testimonals state object property quote */}
+                    <h3 className="testimony__subtitle">{current.client}</h3>
+                </div>
 
 
-                {Object.keys(testimonials).map(index => ( // map through Testimonals and for each position of the array we are going to create a span 
-                    <span className="testimony__span"
+                <div className="testimony__span-container">
 
-                        // style={{ backgroundColor: "red" }}
-                        onClick={event => handleSetClick(event)}
-                        data-quote={index} //custom html attribute- use this to style the span depending on what the active state is 
-                        key={index}
 
-                    />
+                    {testimonials.map((content, index) => ( // map through Testimonals and for each position of the array we are going to create a span 
 
-                ))}
+                        <div className="testimony__span" key={index} onClick={() => handleSetClick(index)} style={index === active ? { backgroundColor: ' #394154', transform: 'scale(1.3)' } : {}} />
+
+                    ))}
+
+                </div>
 
             </div>
         </div>
+
+
     )
 }
 
