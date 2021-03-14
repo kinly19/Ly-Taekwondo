f5bab0252d6b6c398208737fb4f5634c8180949d
unknown <kinchoily@hotmail.com>
2021-03-14 22:35:40 +0000

    major change to component markup, to allow component to be more reusable

--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -10,48 +10,52 @@ const InfoCard = (props) => {
 
     const background = {
         backgroundImage: `url(${props.backgroundImg})`
-        // backgroundPosition: 
     }
 
-    //old 
-
-    // const container = {
-    //     flexDirection: `${props.flexDirection}`
-    // }
-    // const contentStyle = {
-    //     left: `${props.contentLeft}`,
-    //     right: `${props.contentRight}`,
-    //     clipPath: `polygon(${props.contentClipPath})`
-    // }
-
     const headerStyle = {
-        fontSize: `${props.headerFontSize}`
+        fontSize: `${props.headerFontSize}`,
+        color: `${props.headerColor}`
+    }
+
+    const subHeaderStyle = {
+        color: `${props.subHeaderColor}`,
+        marginBottom: `${props.subHeaderMarginB}`
+    }
+
+    const paragraphStyle = {
+        textAlign: `${props.paragraphAlign}`,
+        marginBottom: `${props.paragraphMarginB}`
+    }
+
+    const iconStyle = {
+        display: `${props.iconDisplay}`
     }
 
     return (
 
-        <div className={`infoCard infoCard--${props.className}`}>
+        <div className="infoCard">
 
-            <div className={`infoCard__img infoCard__img--${props.imgClip}`} style={background}> </div>
+            <div className="infoCard__img" style={background}> </div>
 
-            <div className={`infoCard__content infoCard__content--${props.contentPosition}`}>
+            <div className="infoCard__content">
 
-                <h1 className="infoCard__header infoCard__header--1" style={headerStyle}>{props.infoCardTitle}</h1>
-                <h1 className="infoCard__header infoCard__header--2 margin-bottom-xs" >{props.infoCardSubtitle}</h1>
-                <p className="infoCard__paragraph infoCard__paragraph--1 margin-bottom-xs">{props.infoCardText}</p>
-                <p className="infoCard__paragraph infoCard__paragraph--2 margin-bottom-xs">{props.infoCardText2}</p>
+                <h1 className={`infoCard__header infoCard__header--${props.mainHeaderClass}`} style={headerStyle}>{props.infoCardTitle}</h1>
+                <h1 className={`infoCard__header infoCard__header--${props.subHeaderClass} margin-bottom-xs`} style={subHeaderStyle}> {props.infoCardSubtitle}</h1>
+                <p className="infoCard__paragraph infoCard__paragraph margin-bottom-xs" style={paragraphStyle}>{props.infoCardText}</p>
+                <p className="infoCard__paragraph infoCard__paragraph margin-bottom-xs" style={paragraphStyle}>{props.infoCardText2}</p>
+                <p className="infoCard__paragraph infoCard__paragraph margin-bottom-xs" style={paragraphStyle}>{props.infoCardText3}</p>
 
                 <div className="infoCard__btn">
                     <Button
-                        className={props.btnClassName} //dynamic classes 
+                        className={props.btnClassName}
                         link={props.btnLink}
                         title={props.btnTitle}
                     />
                 </div>
 
-                <h1 className="infoCard__header infoCard__header--3 margin-bottom-xs">Dont forget to catch us here</h1> {/**make dynamic? */}
+                <h1 className={`infoCard__header infoCard__header--${props.headerSecondary} margin-bottom-xs`}> {props.infoCardHeader3}</h1> {/**make dynamic? */}
 
-                <div className="infoCard__icon-container">
+                <div className="infoCard__icon-container" style={iconStyle}>
                     <a href="https://www.instagram.com/ly_taekwondo"><img className="infoCard__icons infoCard__icons--instag_icon" src={instagram} alt="instagram icon" /></a>
                     <a href="https://www.facebook.com"> <img className="infoCard__icons infoCard__icons--facebook_icon" src={facebook} alt="sacebook icon" /></a>
                     <a href="https://www.youtube.com/channel/UC4towYpMTj6oK0xVYpMVmvA?"> <img className="infoCard__icons infoCard__icons--youtube_icon" src={youtube2} alt="youtube icon"></img></a>
