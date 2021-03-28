16cbe9d0929a13a06b154970a3748faaaaedc7f2
unknown <kinchoily@hotmail.com>
2021-03-28 19:07:47 +0100

    import scrollToTop component, AboutMe & WhatIsTaekwondo pages. update to navbar switch route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Navbar from './components/Navbar'
+
 import Home from './pages/Home'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop';
+import AboutMe from './pages/AboutMe';
+import WhatIsTaekwondo from './pages/WhatIsTaekwondo/WhatIsTaekwondo';
+
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        < Navbar />
-
+        <ScrollToTop />
+        <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/AboutMe' component={AboutMe} />
+          <Route exact path='/WhatIsTaekwondo' component={WhatIsTaekwondo} />
         </Switch>
       </div>
     </BrowserRouter>
