f54cbf50a44d0516b0df3b89ec429ad5edfa2b25
unknown <kinchoily@hotmail.com>
2021-03-28 19:57:47 +0100

    removed unwanted style, added dynamic class style for title font size

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,23 +9,18 @@ const Landing = (props) => {
         backgroundImage: `linear-gradient(to right, #232323e3, #232323cf),url(${props.lBackgroundImg})`
     }
 
+    const titleStyle = {
+        fontSize: `${props.titleFontSize}`
+    }
+
     return (
 
         <div className="Landing-page" style={landingBackground}>
-            <h1 className="Landing-page__title">{props.lTitle}</h1>
+            <h1 className="landing__title" style={titleStyle}>{props.lTitle}</h1>
             <h2 className="Landing-page__subtitle">{props.lSubtitle}</h2>
         </div>
     )
-
 }
 
+export default Landing;
 
-
-
-
-
-
-
-
-
-export default Landing 
